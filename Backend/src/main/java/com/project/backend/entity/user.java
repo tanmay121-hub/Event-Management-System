3b73eb42a4317fa7f2e4395c86dbf3745ba1a388
Ayushjaiswal1811 <jaiswalamol83@gmail.com>
2026-02-05 16:55:10 +0530

    Dependencies and application.properties

--- Backend/src/main/java/com/project/backend/entity/user.java
+++ Backend/src/main/java/com/project/backend/entity/user.java
@@ -1,8 +1,49 @@
 package com.project.backend.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
 
 @Data
+@Entity
+@Table(name = "user")
+@NoArgsConstructor
+@AllArgsConstructor
 public class user {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
 
+    @Column(nullable = false,length = 100)
+    private String firstName;
+
+    @Column(nullable = false,length = 150,unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 30)
+    private Role role;
+
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
 }
