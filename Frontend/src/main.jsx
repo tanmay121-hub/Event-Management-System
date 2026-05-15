866981bdb797e903411acc852976fc8481728408
Tanmay <patiltanmayanant121@gmail.com>
2026-05-15 18:48:20 +0530

    Fix missing globals.css file

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import App from "./App.jsx";
-import './styles/globals.css'
+import './styles/Globals.css'
 
 const queryClient = new QueryClient();
 
